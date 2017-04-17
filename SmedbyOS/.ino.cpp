675df2df2972c0809c5b100f96522ab115ab1068
ola <ola@ola-virtual-machine>
2017-04-17 10:55:15 +0200

    123

---
+++ SmedbyOS/.ino.cpp
@@ -0,0 +1,15 @@
+#ifdef __IN_ECLIPSE__
+//This is a automatic generated file
+//Please do not modify this file
+//If you touch this file your change will be overwritten during the next build
+//This file has been generated on 2017-04-17 10:49:45
+
+#include "Arduino.h"
+#include "Arduino.h"
+void setup() ;
+void loop() ;
+
+#include "SmedbyOS.ino"
+
+
+#endif
